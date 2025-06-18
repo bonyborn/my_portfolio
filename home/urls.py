9ba76dfc618e1b78aee91a79448099955b99a9a3
Bony Born <ezekielbon2@gmail.com>
2025-06-18 16:10:49 +0000

    modified urls:app level

--- home/urls.py
+++ home/urls.py
@@ -9,8 +9,8 @@ admin.site.index_title="Welcome to this Portal"
 
 urlpatterns=[
     path("", views.home, name="home"),
-    path("about/", views.home, name="about"),
-    path("project", views.home, name="project"),
-    path("contact", views.home, name="contact"),
+    path("about/", views.about, name="about"),
+    path("project/", views.project, name="project"),
+    path("contact/", views.contact, name="contact"),
 ]
 
